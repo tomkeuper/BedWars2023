074166384bee0aab187d8d8898d39d30e9ebb70f
porudev <porudev@gmail.com>
2023-04-18 21:37:25 +0700

    Added respawn message!

--- bedwars-plugin/src/main/java/com/andrei1058/bedwars/language/English.java
+++ bedwars-plugin/src/main/java/com/andrei1058/bedwars/language/English.java
@@ -276,6 +276,7 @@ public class English extends Language {
         yml.addDefault(Messages.PLAYER_DIE_RESPAWN_TITLE, "&cYOU DIED!");
         yml.addDefault(Messages.PLAYER_DIE_RESPAWN_SUBTITLE, "&eYou will respawn in &c{time} &eseconds!");
         yml.addDefault(Messages.PLAYER_DIE_RESPAWN_CHAT, "{prefix}&eYou will respawn in &c{time} &eseconds!");
+        yml.addDefault(Messages.PLAYER_DIE_RESPAWNED_TEXT, "&eYou have respawned!");
         yml.addDefault(Messages.PLAYER_DIE_RESPAWNED_TITLE, "&aRESPAWNED!");
         yml.addDefault(Messages.PLAYER_DIE_ELIMINATED_CHAT, "{prefix}&cYou have been eliminated!");
         yml.addDefault(Messages.PLAYER_HIT_BOW, "{prefix}{TeamColor}{PlayerName} &7is on &c{amount} &7HP!");
