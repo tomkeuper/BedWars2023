6063b5a6dc3196f0b144d10bcdec9280bb3bd69a
Yenil <85287520+ImYenil@users.noreply.github.com>
2023-05-09 21:09:33 +0900

    Changed from upgrades2.yml to upgrades.yml
    Changed from upgrades2.yml to upgrades.yml

--- bedwars-plugin/src/main/java/com/andrei1058/bedwars/upgrades/UpgradesManager.java
+++ bedwars-plugin/src/main/java/com/andrei1058/bedwars/upgrades/UpgradesManager.java
@@ -65,11 +65,7 @@ public class UpgradesManager {
     }
 
     public static void init() {
-        File oldFile = new File(plugin.getDataFolder(), "/upgrades.yml");
-        //noinspection ResultOfMethodCallIgnored
-        oldFile.delete();
-
-        upgrades = new UpgradesConfig("upgrades2", plugin.getDataFolder().getPath());
+        upgrades = new UpgradesConfig("upgrades", plugin.getDataFolder().getPath());
         String name;
         for (String index : upgrades.getYml().getConfigurationSection("").getKeys(false)) {
             name = index;
