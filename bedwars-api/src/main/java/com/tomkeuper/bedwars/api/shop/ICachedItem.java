b9c90fde4a92dfffb4c77bc9dd684e003acb4304
Tom Keuper <tomkeuper@live.nl>
2023-06-07 21:54:22 +0200

    Add documentation ICachedItem

--- bedwars-api/src/main/java/com/tomkeuper/bedwars/api/shop/ICachedItem.java
+++ bedwars-api/src/main/java/com/tomkeuper/bedwars/api/shop/ICachedItem.java
@@ -2,9 +2,31 @@ package com.tomkeuper.bedwars.api.shop;
 
 import org.bukkit.entity.Player;
 
+/**
+ * Represents a cached item in the shop, keeping track of the associated category content and player's tier.
+ */
 public interface ICachedItem {
+
+    /**
+     * Update the item in the specified slot for the given player.
+     *
+     * @param slot The slot to update the item in.
+     * @param p    The player for whom to update the item.
+     */
     void updateItem(int slot, Player p);
+
+    /**
+     * Get the tier of the cached item.
+     *
+     * @return The tier of the cached item.
+     */
     int getTier();
+
+    /**
+     * Upgrade the cached item in the specified slot.
+     *
+     * @param slot The slot of the item to upgrade.
+     */
     void upgrade(int slot);
 
 }
