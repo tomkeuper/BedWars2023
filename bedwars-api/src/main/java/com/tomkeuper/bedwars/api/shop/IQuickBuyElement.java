8815af9b2326e56201ee8401a02b0425c2867159
Tom Keuper <tomkeuper@live.nl>
2023-06-07 21:55:45 +0200

    Add documentation IQuickBuyElement

--- bedwars-api/src/main/java/com/tomkeuper/bedwars/api/shop/IQuickBuyElement.java
+++ bedwars-api/src/main/java/com/tomkeuper/bedwars/api/shop/IQuickBuyElement.java
@@ -2,9 +2,30 @@ package com.tomkeuper.bedwars.api.shop;
 
 import com.tomkeuper.bedwars.api.arena.shop.ICategoryContent;
 
+/**
+ * Represents an element in the quick buy menu, containing a slot, category content, and load status.
+ */
 public interface IQuickBuyElement {
-    boolean isLoaded();
-    int getSlot();
-    ICategoryContent getCategoryContent();
 
+    /**
+     * Check if the quick buy element is loaded.
+     *
+     * @return true if the element is loaded, false otherwise.
+     */
+    boolean isLoaded();
+
+    /**
+     * Get the slot of the quick buy element.
+     *
+     * @return The slot of the element.
+     */
+    int getSlot();
+
+    /**
+     * Get the category content associated with the quick buy element.
+     *
+     * @return The category content of the element.
+     */
+    ICategoryContent getCategoryContent();
 }
+
