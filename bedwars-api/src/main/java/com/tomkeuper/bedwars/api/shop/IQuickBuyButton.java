0f54c3f511f298df081529653444c184f3b8fed3
Tom Keuper <tomkeuper@live.nl>
2023-06-09 23:53:43 +0200

    Add documentation IQuickBuyButton

--- bedwars-api/src/main/java/com/tomkeuper/bedwars/api/shop/IQuickBuyButton.java
+++ bedwars-api/src/main/java/com/tomkeuper/bedwars/api/shop/IQuickBuyButton.java
@@ -3,7 +3,24 @@ package com.tomkeuper.bedwars.api.shop;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
 
+/**
+ * Represents a quick buy button.
+ */
 public interface IQuickBuyButton {
+
+    /**
+     * Get the ItemStack of the quick buy button for a specific player.
+     *
+     * @param player The player for whom to retrieve the button.
+     * @return The ItemStack of the quick buy button.
+     */
     ItemStack getItemStack(Player player);
+
+    /**
+     * Get the slot of the quick buy button in the inventory.
+     *
+     * @return The slot of the quick buy button.
+     */
     int getSlot();
 }
+
