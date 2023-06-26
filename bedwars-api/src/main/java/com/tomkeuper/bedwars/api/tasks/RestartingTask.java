2778caf1b854520c388b0bbefff9cfe2da2e3755
Tom Keuper <tomkeuper@live.nl>
2023-06-26 20:36:04 +0200

    add RestartingTask interface documentation

--- bedwars-api/src/main/java/com/tomkeuper/bedwars/api/tasks/RestartingTask.java
+++ bedwars-api/src/main/java/com/tomkeuper/bedwars/api/tasks/RestartingTask.java
@@ -23,18 +23,41 @@ package com.tomkeuper.bedwars.api.tasks;
 import com.tomkeuper.bedwars.api.arena.IArena;
 import org.bukkit.scheduler.BukkitTask;
 
+/**
+ * The RestartingTask interface represents a task that handles the restarting process of a game in a BedWars arena.
+ */
 public interface RestartingTask {
 
+    /**
+     * Get the arena associated with the task.
+     *
+     * @return The arena object.
+     */
     IArena getArena();
 
+    /**
+     * Get the BukkitTask associated with the task.
+     *
+     * @return The BukkitTask object.
+     */
     BukkitTask getBukkitTask();
 
     /**
-     * Get bukkit task id.
+     * Get the task ID of the task.
+     *
+     * @return The task ID.
      */
     int getTask();
 
+    /**
+     * Get the remaining time for the restarting process.
+     *
+     * @return The remaining time in seconds.
+     */
     int getRestarting();
 
+    /**
+     * Cancel the task.
+     */
     void cancel();
 }
