fad418c5b4a1a58b9aa9ad23caeeeb0b342467ae
MrCeasar <tomkeuper@live.nl>
2023-06-27 13:50:50 +0200

    revert target change

--- bedwars-plugin/src/main/java/com/tomkeuper/bedwars/arena/spectator/TeleporterGUI.java
+++ bedwars-plugin/src/main/java/com/tomkeuper/bedwars/arena/spectator/TeleporterGUI.java
@@ -116,7 +116,7 @@ public class TeleporterGUI {
         IArena currentArena = Arena.getArenaByPlayer(targetPlayer);
         ITeam targetPlayerTeam = currentArena.getTeam(targetPlayer);
 
-        im.setDisplayName(getMsg(GUIholder, Messages.ARENA_SPECTATOR_TELEPORTER_GUI_HEAD_NAME)
+        im.setDisplayName(getMsg(targetPlayer, Messages.ARENA_SPECTATOR_TELEPORTER_GUI_HEAD_NAME)
                 .replace("%bw_v_prefix%", BedWars.getChatSupport().getPrefix(targetPlayer))
                 .replace("%bw_v_suffix%", BedWars.getChatSupport().getSuffix(targetPlayer))
                 .replace("%bw_player%", targetPlayer.getDisplayName())
