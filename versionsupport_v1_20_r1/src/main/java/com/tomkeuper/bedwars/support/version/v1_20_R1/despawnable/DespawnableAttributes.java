6f5cb77205e4a2a556d8bb217b3a60eaef0e7d5e
Tom Keuper <tomkeuper@live.nl>
2023-06-29 17:22:44 +0200

    Implement 1.20 support

---
+++ versionsupport_v1_20_r1/src/main/java/com/tomkeuper/bedwars/support/version/v1_20_R1/despawnable/DespawnableAttributes.java
@@ -0,0 +1,5 @@
+package com.tomkeuper.bedwars.support.version.v1_20_R1.despawnable;
+
+public record DespawnableAttributes(DespawnableType type, double speed, double health, double damage, int despawnSeconds) {
+
+}
