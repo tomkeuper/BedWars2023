2ee64cb8c19fff09e44bbab75065f3773789932f
Tom Keuper <tomkeuper@live.nl>
2023-07-09 20:40:31 +0200

    Fixup AddonManager doc

--- bedwars-api/src/main/java/com/tomkeuper/bedwars/api/addon/IAddonManager.java
+++ bedwars-api/src/main/java/com/tomkeuper/bedwars/api/addon/IAddonManager.java
@@ -3,37 +3,51 @@ package com.tomkeuper.bedwars.api.addon;
 
 import java.util.List;
 
+/**
+ * The manager responsible for handling addons.
+ */
 public interface IAddonManager {
+
     /**
-     * Get the registered addons
+     * Get the list of registered addons.
+     *
+     * @return The list of registered addons.
      */
     List<Addon> getAddons();
 
     /**
-     * Get the loaded addons
+     * Get the list of loaded addons.
+     *
+     * @return The list of loaded addons.
      */
     List<Addon> getLoadedAddons();
 
     /**
-     * Get the unloaded addons
+     * Get the list of unloaded addons.
+     *
+     * @return The list of unloaded addons.
      */
     List<Addon> getUnloadedAddons();
 
     /**
-     * Get the addons made by a specific author
-     * @param author - Input the author of the addon you want to get
+     * Get the list of addons created by a specific author.
+     *
+     * @param author The author of the addons.
+     * @return The list of addons created by the specified author.
      */
     List<Addon> getAddonsByAuthor(String author);
 
     /**
-     * Load an addon
-     * @param addon - Load a specific addon
+     * Load an addon.
+     *
+     * @param addon The addon to load.
      */
     void loadAddon(Addon addon);
 
     /**
-     * Unload an addon
-     * @param addon - Unload a specific addon
+     * Unload an addon.
+     *
+     * @param addon The addon to unload.
      */
     void unloadAddon(Addon addon);
 
@@ -47,5 +61,10 @@ public interface IAddonManager {
      */
     void loadAddons();
 
+    /**
+     * Register an addon.
+     *
+     * @param addon The addon to register.
+     */
     void registerAddon(Addon addon);
 }
