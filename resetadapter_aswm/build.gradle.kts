09f274153cdabbd838c940b9644cc0db8a001a37
Tom Keuper <tomkeuper@live.nl>
2023-08-17 12:02:11 +0200

    Update nbt dependency

--- resetadapter_aswm/build.gradle.kts
+++ resetadapter_aswm/build.gradle.kts
@@ -7,7 +7,7 @@ plugins {
 }
 
 dependencies {
-    api("com.flowpowered:flow-nbt:2.0.0")
+    api("com.flowpowered:flow-nbt:2.0.2")
     implementation(project(":bedwars-api"))
     compileOnly("org.spigotmc:spigot-api:1.8.8-R0.1-SNAPSHOT")
     compileOnly("com.grinderwolf:slimeworldmanager-api:2.8.0-SNAPSHOT")
--- resetadapter_slimepaper/build.gradle.kts
+++ resetadapter_slimepaper/build.gradle.kts
@@ -7,7 +7,7 @@ plugins {
 }
 
 dependencies {
-    api("com.flowpowered:flow-nbt:2.0.0")
+    api("com.flowpowered:flow-nbt:2.0.2")
     implementation(project(":bedwars-api"))
     compileOnly("org.spigotmc:spigot-api:1.8.8-R0.1-SNAPSHOT")
     compileOnly("com.infernalsuite.aswm:api:1.20-R0.1-SNAPSHOT")
