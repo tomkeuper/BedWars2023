0eb7224f7937b7ba46712eedffdf9e804a7df5f4
AgentNoobff <139374594+AgentNoobff@users.noreply.github.com>
2023-11-23 21:07:57 +0530

    More Placeholders!

--- bedwars-plugin/src/main/java/com/tomkeuper/bedwars/support/papi/PAPISupport.java
+++ bedwars-plugin/src/main/java/com/tomkeuper/bedwars/support/papi/PAPISupport.java
@@ -26,10 +26,13 @@ import com.tomkeuper.bedwars.api.arena.IArena;
 import com.tomkeuper.bedwars.api.arena.team.ITeam;
 import com.tomkeuper.bedwars.api.language.Language;
 import com.tomkeuper.bedwars.api.language.Messages;
+import com.tomkeuper.bedwars.api.party.Party;
 import com.tomkeuper.bedwars.api.stats.IPlayerStats;
 import com.tomkeuper.bedwars.arena.Arena;
 import com.tomkeuper.bedwars.commands.shout.ShoutCommand;
 import me.clip.placeholderapi.expansion.PlaceholderExpansion;
+import net.md_5.bungee.api.ChatColor;
+import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 import org.jetbrains.annotations.NotNull;
 
@@ -37,8 +40,12 @@ import java.sql.Timestamp;
 import java.text.SimpleDateFormat;
 import java.time.Duration;
 import java.time.Instant;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
 
 import static com.tomkeuper.bedwars.api.language.Language.getMsg;
+
 
 public class PAPISupport extends PlaceholderExpansion {
 
@@ -64,7 +71,9 @@ public class PAPISupport extends PlaceholderExpansion {
     public boolean persist() {
         return true;
     }
-
+    
+    private final SimpleDateFormat nextEventFormat = new SimpleDateFormat("mm:ss");
+    
     @Override
     public String onPlaceholderRequest(Player player, @NotNull String s) {
 
@@ -110,6 +119,10 @@ public class PAPISupport extends PlaceholderExpansion {
         /* Player required placeholders */
         if (player == null) return null;
 
+        /* Arena required placeholders */
+        IArena arena = BedWars.getAPI().getArenaUtil().getArenaByPlayer(player);
+        if (arena == null) return "";
+        
         // stats placeholders
         if(s.startsWith("stats_")) {
             String targetedStat = s.replaceFirst("stats_", "");
@@ -148,6 +161,142 @@ public class PAPISupport extends PlaceholderExpansion {
             }
         }
 
+        // party placeholders
+        Party party = BedWars.getAPI().getPartyUtil();
+        if (s.equalsIgnoreCase("party_has"))
+            return String.valueOf(party.hasParty(player));
+        if (s.startsWith("party_members")) {
+            StringBuilder output = new StringBuilder();
+            List<Player> list = new ArrayList<>(party.getMembers(player));
+            if (s.equalsIgnoreCase("party_members_amount"))
+                output = new StringBuilder(String.valueOf(list.size()));
+            else if (s.equalsIgnoreCase("party_members")) {
+                for (Player pl : list) {
+                    output.append(pl.getName());
+                    if (list.indexOf(pl) != list.size() - 1) output.append(", ");
+                }
+            }
+            return output.toString();
+        }
+        if (s.startsWith("party_in_yours_")) {
+            Player p2 = Bukkit.getServer().getPlayer(s.replace("party_in_yours_",""));
+            if (p2 == null) return "false";
+            return String.valueOf(party.isMember(player, p2));
+        }
+        if (s.startsWith("party_in_his_")) {
+            Player p2 = Bukkit.getServer().getPlayer(s.replace("party_in_his_",""));
+            if (p2 == null) return "false";
+            return String.valueOf(party.isMember(p2, player));
+        }
+        if (s.startsWith("party_is_owner")) {
+            if (s.equalsIgnoreCase("party_is_owner")) return String.valueOf(party.isOwner(player));
+            if (s.startsWith("party_is_owner_")) {
+                Player p2 = Bukkit.getServer().getPlayer(s.replace("party_is_owner_", ""));
+                if (p2 == null) return "false";
+                return String.valueOf(party.isOwner(p2));
+            }
+        }
+
+        Language lang = BedWars.getAPI().getPlayerLanguage(player);
+        if (s.startsWith("lang"))
+            return lang.getLangName();
+
+        // team placeholders
+        if (s.startsWith("team_status")) {
+            ITeam team;
+            if (s.equalsIgnoreCase("team_status"))
+                team = arena.getTeam(player);
+            else team = arena.getTeam(s.replace("team_status_", ""));
+            if (team != null)
+                return !team.isBedDestroyed() ? lang.getString("format-sb-team-alive") : !team.getMembers().isEmpty() ? String.valueOf(team.getMembers().size()) : lang.getString("format-sb-team-eliminated");
+        }
+        if (s.startsWith("team_color")) {
+            ITeam team;
+            if (s.equalsIgnoreCase("team_color"))
+                team = arena.getTeam(player);
+            else team = arena.getTeam(s.replace("team_color_", ""));
+            if (team != null)
+                return team.getColor().chat().toString();
+        }
+        if (s.startsWith("team_letter")) {
+            ITeam team;
+            if (s.equalsIgnoreCase("team_letter"))
+                team = arena.getTeam(player);
+            else team = arena.getTeam(s.replace("team_letter_", ""));
+            if (team != null)
+                return team.getName().substring(0,1).toUpperCase();
+        }
+        if (s.startsWith("team_players_amount")) {
+            ITeam team;
+            if (s.equalsIgnoreCase("team_players_amount"))
+                team = arena.getTeam(player);
+            else team = arena.getTeam(s.replace("team_players_amount_", ""));
+            if (team != null)
+                return String.valueOf(team.getMembers().size());
+        }
+        if (s.startsWith("team_players")) {
+            ITeam team;
+            if (s.equalsIgnoreCase("team_players"))
+                team = arena.getTeam(player);
+            else team = arena.getTeam(s.replace("team_players_", ""));
+            if (team != null) {
+                StringBuilder output = new StringBuilder();
+                List<Player> list = new ArrayList<>(team.getMembers());
+                for (Player pl : list) {
+                    output.append(pl.getName());
+                    if (list.indexOf(pl) != list.size()-1) output.append(", ");
+                }
+                return output.toString();
+            }
+        }
+
+        // arena info placeholders
+        switch (s) {
+            case "arena_nextevent_name":
+                return arena.getNextEvent().toString().toLowerCase().replace("_", " ");
+            case "arena_nextevent_time":
+                return String.valueOf(getNextEventTime(arena));
+            case "arena_nextevent_time_formatted":
+                return nextEventFormat.format(new Date(getNextEventTime(arena) * 1000L));
+            case "arena_name":
+                return arena.getArenaName();
+            case "arena_display_name":
+                return arena.getDisplayName();
+            case "arena_group":
+                return arena.getGroup();
+            case "arena_world":
+                return arena.getWorldName();
+            case "arena_status_plocale":
+                return arena.getDisplayStatus(lang);
+            case "arena_status":
+                return arena.getDisplayStatus(BedWars.getAPI().getDefaultLang());
+        }
+        
+        // inside arena stats placeholders
+        switch (s) {
+            case "player_kills": return String.valueOf(arena.getPlayerKills(player, false));
+            case "player_kills_total": return String.valueOf(arena.getPlayerKills(player,true)+arena.getPlayerKills(player,false));
+            case "player_kills_final": return String.valueOf(arena.getPlayerKills(player, true));
+            case "player_deaths": return String.valueOf(arena.getPlayerDeaths(player, false));
+            case "player_deaths_total": return String.valueOf(arena.getPlayerDeaths(player,true)+arena.getPlayerDeaths(player,false));
+            case "player_deaths_final": return String.valueOf(arena.getPlayerDeaths(player, true));
+            case "player_beds": return String.valueOf(arena.getPlayerBedsDestroyed(player));
+            case "status_color": return (arena.isSpectator(player) ? ChatColor.GRAY : arena.getTeam(player).getColor().chat()).toString();
+            case "status_letter": return arena.isSpectator(player) ? getSpectatorLetter(lang) : arena.getTeam(player).getName().substring(0,1).toUpperCase();
+        }
+        if (s.startsWith("players")) {
+            StringBuilder output = new StringBuilder();
+            List<Player> list = new ArrayList<>(arena.getPlayers());
+            if (s.equalsIgnoreCase("players_amount"))
+                output = new StringBuilder(String.valueOf(list.size()));
+            else if (s.equalsIgnoreCase("players")) {
+                for (Player pl : list) {
+                    output.append(pl.getName());
+                    if (list.indexOf(pl) != list.size() - 1) output.append(", ");
+                }
+            }
+            return output.toString();
+        }
         // other placeholders
         String response = "";
         IArena a = Arena.getArenaByPlayer(player);
@@ -232,7 +381,7 @@ public class PAPISupport extends PlaceholderExpansion {
                             } else if(a.isSpectator(player)) {
                                 response = "SPECTATING";
                             } else {
-                                response = "IN_GAME_BUT_NOT"; // this shouldnt happen
+                                response = "IN_GAME_BUT_NOT"; // this shouldn't happen
                             }
                             break;
                         case restarting:
@@ -263,5 +412,23 @@ public class PAPISupport extends PlaceholderExpansion {
                 break;
         }
         return response;
+    }
+
+    private String getSpectatorLetter(Language lang) {
+        String letter = lang.m("format-papi-player-spectator-letter");
+        return letter.equals("MISSING_LANG") ? "S" : letter;
+    }
+    
+    private int getNextEventTime(IArena arena) {
+        switch (arena.getNextEvent()) {
+            case EMERALD_GENERATOR_TIER_II:
+            case EMERALD_GENERATOR_TIER_III: return arena.getUpgradeEmeraldsCount();
+            case DIAMOND_GENERATOR_TIER_II:
+            case DIAMOND_GENERATOR_TIER_III: return arena.getUpgradeDiamondsCount();
+            case BEDS_DESTROY: return arena.getPlayingTask().getBedsDestroyCountdown();
+            case ENDER_DRAGON: return arena.getPlayingTask().getDragonSpawnCountdown();
+            case GAME_END: return arena.getPlayingTask().getGameEndCountdown();
+            default: return 0;
+        }
     }
 }
