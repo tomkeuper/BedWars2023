aa9e18a53c2091181b4f8e5814c621bb2a06e4b8
MrCeasar <tomkeuper@live.nl>
2024-01-26 09:52:00 +0100

    Force update prefix + suffix on arena leave

--- bedwars-plugin/src/main/java/com/tomkeuper/bedwars/sidebar/BoardListener.java
+++ bedwars-plugin/src/main/java/com/tomkeuper/bedwars/sidebar/BoardListener.java
@@ -3,7 +3,9 @@ package com.tomkeuper.bedwars.sidebar;
 import com.tomkeuper.bedwars.api.events.player.PlayerLeaveArenaEvent;
 import com.tomkeuper.bedwars.arena.Arena;
 import me.neznamy.tab.api.TabAPI;
+import me.neznamy.tab.api.TabPlayer;
 import me.neznamy.tab.api.bossbar.BossBar;
+import me.neznamy.tab.api.placeholder.PlayerPlaceholder;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 
@@ -17,5 +19,14 @@ public class BoardListener implements Listener {
                 bossBar.removePlayer(Objects.requireNonNull(TabAPI.getInstance().getPlayer(event.getPlayer().getUniqueId())));
             }
         }
+
+        // Force update the prefix and suffix
+        PlayerPlaceholder prefixPlaceholder = (PlayerPlaceholder) TabAPI.getInstance().getPlaceholderManager().getPlaceholder("%bw_prefix%");
+        PlayerPlaceholder suffixPlaceholder = (PlayerPlaceholder) TabAPI.getInstance().getPlaceholderManager().getPlaceholder("%bw_suffix%");
+        TabPlayer tabPlayer = TabAPI.getInstance().getPlayer(event.getPlayer().getUniqueId());
+
+        assert tabPlayer != null;
+        prefixPlaceholder.updateValue(tabPlayer, BoardManager.getInstance().getPrefix(tabPlayer));
+        suffixPlaceholder.updateValue(tabPlayer, BoardManager.getInstance().getSuffix(tabPlayer));
     }
 }
