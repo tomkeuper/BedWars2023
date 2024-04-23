5dc2412c4946699dd2f28a29a53a816b794b686f
Wanbin Cao <lagggpixel@gmail.com>
2024-04-23 14:56:46 +0100

    feat: implemented PlayerStats.java to IPlayerStats.java modifications

--- bedwars-plugin/src/main/java/com/tomkeuper/bedwars/stats/PlayerStats.java
+++ bedwars-plugin/src/main/java/com/tomkeuper/bedwars/stats/PlayerStats.java
@@ -23,6 +23,8 @@ package com.tomkeuper.bedwars.stats;
 import com.tomkeuper.bedwars.api.stats.IPlayerStats;
 
 import java.time.Instant;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.UUID;
 
 public class PlayerStats implements IPlayerStats {
@@ -32,115 +34,219 @@ public class PlayerStats implements IPlayerStats {
     private String name;
     private Instant firstPlay;
     private Instant lastPlay;
-    private int wins;
-    private int kills;
-    private int finalKills;
-    private int totalKills;
-    private int losses;
-    private int deaths;
-    private int finalDeaths;
-    private int bedsDestroyed;
-    private int gamesPlayed;
+    private final Map<String, Integer> wins;
+    private final Map<String, Integer> kills;
+    private final Map<String, Integer> finalKills;
+    private final Map<String, Integer> losses;
+    private final Map<String, Integer> deaths ;
+    private final Map<String, Integer> finalDeaths;
+    private final Map<String, Integer> bedsDestroyed;
+    private final Map<String, Integer> gamesPlayed;
 
     public PlayerStats(UUID uuid) {
         this.uuid = uuid;
+        this.wins = new HashMap<>();
+        this.kills = new HashMap<>();
+        this.finalKills = new HashMap<>();
+        this.losses = new HashMap<>();
+        this.deaths = new HashMap<>();
+        this.finalDeaths = new HashMap<>();
+        this.bedsDestroyed = new HashMap<>();
+        this.gamesPlayed = new HashMap<>();
     }
 
+    @Override
     public String getName() {
         return name;
     }
 
+    @Override
     public void setName(String name) {
         this.name = name;
     }
 
+    @Override
     public UUID getUuid() {
         return uuid;
     }
 
+    @Override
     public Instant getFirstPlay() {
         return firstPlay;
     }
 
+    @Override
     public void setFirstPlay(Instant firstPlay) {
         this.firstPlay = firstPlay;
     }
 
+    @Override
     public Instant getLastPlay() {
         return lastPlay;
     }
 
+    @Override
     public void setLastPlay(Instant lastPlay) {
         this.lastPlay = lastPlay;
     }
 
+    @Override
     public int getWins() {
-        return wins;
+        return wins.values().stream().mapToInt(i -> i).sum();
     }
 
-    public void setWins(int wins) {
-        this.wins = wins;
+    @Override
+    public int getWins(String group) {
+        return wins.getOrDefault(group, 0);
     }
 
+    @Override
+    public void setWins(String group, int value) {
+        if (wins.containsKey(group)) {
+            wins.replace(group, value);
+        } else {
+            wins.put(group, value);
+        }
+    }
+
+    @Override
     public int getKills() {
-        return kills;
+        return kills.values().stream().mapToInt(i -> i).sum();
     }
 
-    public void setKills(int kills) {
-        this.kills = kills;
-        this.totalKills = kills + finalKills;
+    @Override
+    public int getKills(String group) {
+        return kills.getOrDefault(group, 0);
     }
 
+    @Override
+    public void setKills(String group, int value) {
+        if (kills.containsKey(group)) {
+            kills.replace(group, value);
+        } else {
+            kills.put(group, value);
+        }
+    }
+
+    @Override
     public int getFinalKills() {
-        return finalKills;
+        return finalKills.values().stream().mapToInt(i -> i).sum();
     }
 
-    public void setFinalKills(int finalKills) {
-        this.finalKills = finalKills;
-        this.totalKills = kills + finalKills;
+    @Override
+    public int getFinalKills(String group) {
+        return finalKills.getOrDefault(group, 0);
     }
 
+    @Override
+    public void setFinalKills(String group, int value) {
+        if (finalKills.containsKey(group)) {
+            finalKills.replace(group, value);
+        } else {
+            finalKills.put(group, value);
+        }
+    }
+
+    @Override
     public int getLosses() {
-        return losses;
+        return losses.values().stream().mapToInt(i -> i).sum();
     }
 
-    public void setLosses(int losses) {
-        this.losses = losses;
+    @Override
+    public int getLosses(String group) {
+        return losses.getOrDefault(group, 0);
+    }
+
+    @Override
+    public void setLosses(String group, int value) {
+        if (losses.containsKey(group)) {
+            losses.replace(group, value);
+        } else {
+            losses.put(group, value);
+        }
     }
 
     public int getDeaths() {
-        return deaths;
+        return deaths.values().stream().mapToInt(i -> i).sum();
     }
 
-    public void setDeaths(int deaths) {
-        this.deaths = deaths;
+    @Override
+    public int getDeaths(String group) {
+        return deaths.getOrDefault(group, 0);
     }
 
+    @Override
+    public void setDeaths(String group, int value) {
+        if (deaths.containsKey(group)) {
+            deaths.replace(group, value);
+        } else {
+            deaths.put(group, value);
+        }
+    }
+
+    @Override
     public int getFinalDeaths() {
-        return finalDeaths;
+        return finalDeaths.values().stream().mapToInt(i -> i).sum();
     }
 
-    public void setFinalDeaths(int finalDeaths) {
-        this.finalDeaths = finalDeaths;
+    @Override
+    public int getFinalDeaths(String group) {
+        return finalDeaths.getOrDefault(group, 0);
     }
 
+    @Override
+    public void setFinalDeaths(String group, int value) {
+        if (finalDeaths.containsKey(group)) {
+            finalDeaths.replace(group, value);
+        } else {
+            finalDeaths.put(group, value);
+        }
+    }
+
+    @Override
     public int getBedsDestroyed() {
-        return bedsDestroyed;
+        return bedsDestroyed.values().stream().mapToInt(i -> i).sum();
     }
 
-    public void setBedsDestroyed(int bedsDestroyed) {
-        this.bedsDestroyed = bedsDestroyed;
+    @Override
+    public int getBedsDestroyed(String group) {
+        return bedsDestroyed.getOrDefault(group, 0);
     }
 
+    @Override
+    public void setBedsDestroyed(String group, int value) {
+        if (bedsDestroyed.containsKey(group)) {
+            bedsDestroyed.replace(group, value);
+        } else {
+            bedsDestroyed.put(group, value);
+        }
+    }
+
+    @Override
     public int getGamesPlayed() {
-        return gamesPlayed;
+        return gamesPlayed.values().stream().mapToInt(i -> i).sum();
     }
 
-    public void setGamesPlayed(int gamePlayed) {
-        this.gamesPlayed = gamePlayed;
+    @Override
+    public int getGamesPlayed(String group) {
+        return gamesPlayed.getOrDefault(group, 0);
+    }
+
+    @Override
+    public void setGamesPlayed(String group, int value) {
+        if (gamesPlayed.containsKey(group)) {
+            gamesPlayed.replace(group, value);
+        } else {
+            gamesPlayed.put(group, value);
+        }
     }
 
     public int getTotalKills() {
-        return totalKills;
+        return getKills() + getFinalKills();
+    }
+
+    @Override
+    public int getTotalKills(String group) {
+        return getKills(group) + getFinalKills();
     }
 }
