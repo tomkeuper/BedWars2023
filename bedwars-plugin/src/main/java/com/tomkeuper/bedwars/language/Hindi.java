a5fc70fd0095b189c6ade38f100a775c8090dc54
MrCeasar <tomkeuper@live.nl>
2024-04-25 02:59:30 +0200

    Update license information

--- bedwars-plugin/src/main/java/com/tomkeuper/bedwars/language/Hindi.java
+++ bedwars-plugin/src/main/java/com/tomkeuper/bedwars/language/Hindi.java
@@ -1,6 +1,6 @@
 /*
- * BedWars1058 - A bed wars mini-game.
- * Copyright (C) 2021 Andrei Dascălu
+ * BedWars2023 - A bed wars mini-game.
+ * Copyright (C) 2024 Tomas Keuper
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -15,7 +15,7 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <https://www.gnu.org/licenses/>.
  *
- * Contact e-mail: andrew.dascalu@gmail.com
+ * Contact e-mail: contact@fyreblox.com
  */
 // This Language file was created by Monzu77, contact on discord for any questions regarding this language file. Dicsord: Itsmemonzu#6732
 package com.tomkeuper.bedwars.language;
