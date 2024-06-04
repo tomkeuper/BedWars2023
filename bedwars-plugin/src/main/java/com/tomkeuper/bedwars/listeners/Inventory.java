a857cc7d8f1f1405c159a42b6563b55c55b27b4e
MrCeasar <tomkeuper@live.nl>
2024-06-05 00:39:10 +0200

    Add inventory event import

--- bedwars-plugin/src/main/java/com/tomkeuper/bedwars/listeners/Inventory.java
+++ bedwars-plugin/src/main/java/com/tomkeuper/bedwars/listeners/Inventory.java
@@ -41,10 +41,7 @@ import org.bukkit.Material;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
-import org.bukkit.event.inventory.ClickType;
-import org.bukkit.event.inventory.InventoryClickEvent;
-import org.bukkit.event.inventory.InventoryCloseEvent;
-import org.bukkit.event.inventory.InventoryType;
+import org.bukkit.event.inventory.*;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.potion.PotionEffectType;
 
