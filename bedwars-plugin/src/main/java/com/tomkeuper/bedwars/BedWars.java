53a05d764bfe9a00f5ae3af6b00fca9d193265c3
MrCeasar <tomkeuper@live.nl>
2024-07-03 16:44:40 +0200

    Add versions to nms parser

--- bedwars-plugin/src/main/java/com/tomkeuper/bedwars/BedWars.java
+++ bedwars-plugin/src/main/java/com/tomkeuper/bedwars/BedWars.java
@@ -224,6 +224,12 @@ public class BedWars extends JavaPlugin {
             case "1.20.4":
                 nmsVersion = "v1_20_R3";
                 break;
+            case "1.20.5":
+                nmsVersion = "v1_20_R4";
+                break;
+            case "1.21":
+                nmsVersion = "v1_21_R1";
+                break;
             default:
                 break;
         }
