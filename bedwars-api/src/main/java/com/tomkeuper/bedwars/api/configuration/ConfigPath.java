f33bc403d32ff716032008cb54b51662842b975d
Samuel Henrique <150966678+zyypj@users.noreply.github.com>
2025-06-10 10:57:36 -0300

    refactor(): consolidate click-in-chest-to-deposit feature into nested config section

--- bedwars-api/src/main/java/com/tomkeuper/bedwars/api/configuration/ConfigPath.java
+++ bedwars-api/src/main/java/com/tomkeuper/bedwars/api/configuration/ConfigPath.java
@@ -79,8 +79,10 @@ public class ConfigPath {
     public static final String GENERAL_CONFIGURATION_ENABLE_ANTI_DROP = "enable-anti-drop-feature";
     public static final String GENERAL_CONFIGURATION_HOLOGRAM_UPDATE_DISTANCE = "hologram-update-distance";
     public static final String GENERAL_CONFIGURATION_ENABLE_TEAMMATE_TRACKING_ACTION_BAR = "enable-teammate-tracking-action-bar";
-    public static final String GENERAL_CONFIGURATION_RESOURCE_CHEST_ENABLED = "enable-click-in-chest-to-deposit-feature";
-    public static final String GENERAL_CONFIGURATION_RESOURCE_CHEST_BLOCKED = "click-in-chest-to-deposit-blocked-items";
+
+    public static final String GENERAL_CONFIGURATION_RESOURCE_CHEST_PREFIX = "click-in-chest-to-deposit-feature.";
+    public static final String GENERAL_CONFIGURATION_RESOURCE_CHEST_ENABLED = GENERAL_CONFIGURATION_RESOURCE_CHEST_PREFIX + "enable";
+    public static final String GENERAL_CONFIGURATION_RESOURCE_CHEST_BLOCKED = GENERAL_CONFIGURATION_RESOURCE_CHEST_PREFIX + "blocked-items";
 
     public static final String GENERAL_CONFIGURATION_LOBBY_ITEMS_PATH = "lobby-items";
     public static final String GENERAL_CONFIGURATION_EXPERIMENTAL_TEAM_ASSIGNER = "use-experimental-team-assigner";
