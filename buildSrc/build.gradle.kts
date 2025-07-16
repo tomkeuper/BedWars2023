58b1a914fd249a9068bb555f5be6ed424a88c7d6
Tom Keuper <tomkeuper@live.nl>
2025-07-16 18:25:41 +0200

    Update Shadow plugin to version 9.0.0-beta6
    Upgraded the Shadow Gradle plugin from 8.3.8 to 9.0.0-beta6 to ensure compatibility with the latest features and fixes. This change aims to improve build stability and align with upstream updates.

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -4,6 +4,6 @@ plugins {
 }
 
 dependencies {
-    implementation("com.gradleup.shadow:shadow-gradle-plugin:8.3.8")
+    implementation("com.gradleup.shadow:shadow-gradle-plugin:9.0.0-beta6")
     implementation("io.freefair.gradle:lombok-plugin:8.6")
 }
