917d4b3f1c95e010e5fa3dd02bc9283274f966c7
Dieu <info@l4b.org>
2025-07-23 00:23:24 +0200

    Fix version mixup

--- bedwars-plugin/src/main/java/com/tomkeuper/bedwars/BedWars.java
+++ bedwars-plugin/src/main/java/com/tomkeuper/bedwars/BedWars.java
@@ -241,12 +241,9 @@ public class BedWars extends JavaPlugin {
                 nmsVersion = "v1_21_R2";
                 break;
             case "1.21.4":
-                nmsVersion = "v1_21_R4";
+                nmsVersion = "v1_21_R3";
                 break;
-            case "1.21.5":
-            case "1.21.6":
             case "1.21.7":
-            case "1.21.8":
                 nmsVersion = "v1_21_R5";
                 break;
             default:
