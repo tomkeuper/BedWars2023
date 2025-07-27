3f1b7cf252f275b9cc14dfd5a6c3d124562b0bd1
Dieu <info@l4b.org>
2025-07-27 17:45:45 +0200

    1.21.6 1.21.7 1.21.8 share same NMS

--- bedwars-plugin/src/main/java/com/tomkeuper/bedwars/BedWars.java
+++ bedwars-plugin/src/main/java/com/tomkeuper/bedwars/BedWars.java
@@ -245,6 +245,7 @@ public class BedWars extends JavaPlugin {
                 break;
             case "1.21.6":
             case "1.21.7":
+            case "1.21.8":
                 nmsVersion = "v1_21_R5";
                 break;
             default:
