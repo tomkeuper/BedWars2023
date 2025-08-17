1816ef570aa1e15b5bc3d25e8062ab50d172df80
MrCeasar <tomkeuper@live.nl>
2025-08-17 18:55:39 +0200

    Log IllegalArgumentException during Fireball velocity calculation

--- bedwars-plugin/src/main/java/com/tomkeuper/bedwars/listeners/FireballListener.java
+++ bedwars-plugin/src/main/java/com/tomkeuper/bedwars/listeners/FireballListener.java
@@ -32,6 +32,7 @@ import com.tomkeuper.bedwars.api.language.Messages;
 import com.tomkeuper.bedwars.arena.Arena;
 import com.tomkeuper.bedwars.arena.LastHit;
 import com.tomkeuper.bedwars.arena.team.BedWarsTeam;
+import org.bukkit.Bukkit;
 import org.bukkit.Location;
 import org.bukkit.World;
 import org.bukkit.configuration.file.YamlConfiguration;
@@ -170,7 +171,9 @@ public class FireballListener implements Listener {
             try {
                 player.setVelocity(horizontalVector.setY(y));
             } catch (IllegalArgumentException ex) {
-                // TODO: implement proper fix for Caused by: java.lang.IllegalArgumentException: x not finite
+                // TODO: implement proper fix for Caused by: java.lang.IllegalArgumentException: x not finite - Logging now!
+                Bukkit.getLogger().severe("IllegalArgumentException has been caught! Horizontal vector is: " + horizontalVector + " with Y being: " + y);
+                Bukkit.getLogger().severe("Player is: " + player.getName() + " and source is: " + source.getName() + " at " + player.getLocation());
             }
 
             LastHit lh = LastHit.getLastHit(player);
