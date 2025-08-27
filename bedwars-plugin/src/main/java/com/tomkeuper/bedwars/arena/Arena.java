05f19dd59b54cfdc7aa036b33d9456220df57f68
Dieu <info@l4b.org>
2025-08-27 23:15:51 +0200

    Fix Arena#addPlayer ConcurrentModificationException
    com.destroystokyo.paper.exception.ServerEventException: Could not pass event PlayerInteractEntityEvent to BedWars2023 v2.0.0-SNAPSHOT
    	at io.papermc.paper.plugin.manager.PaperEventManager.callEvent(PaperEventManager.java:72)
    	at io.papermc.paper.plugin.manager.PaperPluginManagerImpl.callEvent(PaperPluginManagerImpl.java:131)
    	at org.bukkit.plugin.SimplePluginManager.callEvent(SimplePluginManager.java:628)
    	at net.minecraft.server.network.ServerGamePacketListenerImpl$1.performInteraction(ServerGamePacketListenerImpl.java:2775)
    	at net.minecraft.server.network.ServerGamePacketListenerImpl$1.onInteraction(ServerGamePacketListenerImpl.java:2823)
    	at net.minecraft.network.protocol.game.ServerboundInteractPacket$InteractionAction.dispatch(ServerboundInteractPacket.java:140)
    	at net.minecraft.network.protocol.game.ServerboundInteractPacket.dispatch(ServerboundInteractPacket.java:91)
    	at net.minecraft.server.network.ServerGamePacketListenerImpl.handleInteract(ServerGamePacketListenerImpl.java:2765)
    	at net.minecraft.network.protocol.game.ServerboundInteractPacket.handle(ServerboundInteractPacket.java:78)
    	at net.minecraft.network.protocol.game.ServerboundInteractPacket.handle(ServerboundInteractPacket.java:14)
    	at net.minecraft.network.protocol.PacketUtils.lambda$ensureRunningOnSameThread$0(PacketUtils.java:29)
    	at net.minecraft.server.TickTask.run(TickTask.java:18)
    	at net.minecraft.util.thread.BlockableEventLoop.doRunTask(BlockableEventLoop.java:155)
    	at net.minecraft.util.thread.ReentrantBlockableEventLoop.doRunTask(ReentrantBlockableEventLoop.java:24)
    	at net.minecraft.server.MinecraftServer.doRunTask(MinecraftServer.java:1467)
    	at net.minecraft.server.MinecraftServer.doRunTask(MinecraftServer.java:176)
    	at net.minecraft.util.thread.BlockableEventLoop.pollTask(BlockableEventLoop.java:129)
    	at net.minecraft.server.MinecraftServer.pollTaskInternal(MinecraftServer.java:1447)
    	at net.minecraft.server.MinecraftServer.pollTask(MinecraftServer.java:1441)
    	at net.minecraft.util.thread.BlockableEventLoop.runAllTasks(BlockableEventLoop.java:118)
    	at net.minecraft.server.MinecraftServer.waitUntilNextTick(MinecraftServer.java:1402)
    	at net.minecraft.server.MinecraftServer.runServer(MinecraftServer.java:1283)
    	at net.minecraft.server.MinecraftServer.lambda$spin$2(MinecraftServer.java:311)
    	at java.base/java.lang.Thread.run(Thread.java:1583)
    Caused by: java.util.ConcurrentModificationException
    	at java.base/java.util.ArrayList$Itr.checkForComodification(ArrayList.java:1095)
    	at java.base/java.util.ArrayList$Itr.next(ArrayList.java:1049)
    	at BedWars2023.jar//com.tomkeuper.bedwars.arena.Arena.addPlayer(Arena.java:478)
    	at BedWars2023.jar//com.tomkeuper.bedwars.arena.Arena.joinRandomFromGroup(Arena.java:2396)
    	at BedWars2023.jar//com.tomkeuper.bedwars.support.citizens.CitizensListener.onNPCInteract(CitizensListener.java:81)
    	at co.aikar.timings.TimedEventExecutor.execute(TimedEventExecutor.java:80)
    	at org.bukkit.plugin.RegisteredListener.callEvent(RegisteredListener.java:71)
    	at io.papermc.paper.plugin.manager.PaperEventManager.callEvent(PaperEventManager.java:54)
    	... 23 more

--- bedwars-plugin/src/main/java/com/tomkeuper/bedwars/arena/Arena.java
+++ bedwars-plugin/src/main/java/com/tomkeuper/bedwars/arena/Arena.java
@@ -475,8 +475,9 @@ public class Arena implements IArena {
                     p.sendMessage(getMsg(p, Messages.COMMAND_JOIN_DENIED_PARTY_TOO_BIG));
                     return false;
                 }
-                for (Player mem : getPartyManager().getMembers(p)) { // TODO ConcurrentModificationException
-                    if (mem == p) continue;
+                for (Player mem : new ArrayList<>(getPartyManager().getMembers(p))) {
+                    if (mem == p)
+                        continue;
                     IArena a = Arena.getArenaByPlayer(mem);
                     if (a != null) {
                         /*if (a.isPlayer(mem)) {
