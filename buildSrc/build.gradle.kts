77b312e18813f819f727a4cf86022ee45a6727ee
Tom Keuper <tomkeuper@live.nl>
2025-10-28 20:17:07 +0100

    Convert Gradle build scripts from Kotlin DSL to Groovy DSL.

--- buildSrc/build.gradle.kts
+++
@@ -1,9 +0,0 @@
-plugins {
-    // Support convention plugins written in Kotlin. Convention plugins are build scripts in 'src/main' that automatically become available as plugins in the main build.
-    `kotlin-dsl`
-}
-
-dependencies {
-    implementation("com.gradleup.shadow:shadow-gradle-plugin:9.0.0-beta6")
-    implementation("io.freefair.gradle:lombok-plugin:8.6")
-}
