dd6ac3c9bb10b8f7f2697c9bf8b90d397961c1fc
abbflaabb <113551009+abbflaabb@users.noreply.github.com>
2025-11-06 15:39:38 +0300

    Update Sounds.java

--- bedwars-plugin/src/main/java/com/tomkeuper/bedwars/configuration/Sounds.java
+++ bedwars-plugin/src/main/java/com/tomkeuper/bedwars/configuration/Sounds.java
@@ -1,6 +1,4 @@
 /*
- * BedWars1058 - A bed wars mini-game.
- * Copyright (C) 2021 Andrei Dascălu
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
