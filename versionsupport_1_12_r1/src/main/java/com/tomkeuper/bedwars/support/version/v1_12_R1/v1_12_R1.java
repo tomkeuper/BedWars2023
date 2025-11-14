66c85596cf565f59e5f2f0303907774320b7fd6d
Kiiya <a28894@svalero.com>
2025-11-14 10:34:45 +0100

    - Use World#playSound instead of packet to play hurt sound

--- versionsupport_1_12_r1/src/main/java/com/tomkeuper/bedwars/support/version/v1_12_R1/v1_12_R1.java
+++ versionsupport_1_12_r1/src/main/java/com/tomkeuper/bedwars/support/version/v1_12_R1/v1_12_R1.java
@@ -157,13 +157,12 @@ public class v1_12_R1 extends VersionSupport {
     @Override
     public void fakeDamagePlayer(Player e) {
         Location loc = e.getLocation();
-        SoundEffect hurtSound = CraftSound.getSoundEffect(CraftSound.getSound(Sound.ENTITY_PLAYER_HURT));
+        World world = e.getWorld();
+        world.playSound(loc, Sound.ENTITY_PLAYER_HURT, 1.0f, 1.0f);
         PacketPlayOutAnimation anim = new PacketPlayOutAnimation(((CraftPlayer) e).getHandle(), 1);
-        PacketPlayOutNamedSoundEffect sound = new PacketPlayOutNamedSoundEffect(hurtSound, SoundCategory.PLAYERS, loc.getX(), loc.getY(), loc.getZ(), 1.0F, 1.0F);
         for (Player p : e.getWorld().getPlayers()) {
             PlayerConnection pc = ((CraftPlayer) p).getHandle().playerConnection;
             pc.sendPacket(anim);
-            pc.sendPacket(sound);
         }
     }
 
--- versionsupport_1_8_r3/src/main/java/com/tomkeuper/bedwars/support/version/v1_8_R3/v1_8_R3.java
+++ versionsupport_1_8_r3/src/main/java/com/tomkeuper/bedwars/support/version/v1_8_R3/v1_8_R3.java
@@ -146,13 +146,11 @@ public class v1_8_R3 extends VersionSupport {
     public void fakeDamagePlayer(Player e) {
         Location loc = e.getLocation();
         World world = loc.getWorld();
-        Sound hurtSound = Sound.HURT_FLESH;
+        world.playSound(loc, Sound.HURT_FLESH, 1.0f, 1.0f);
         PacketPlayOutAnimation anim = new PacketPlayOutAnimation(((CraftPlayer) e).getHandle(), 1);
-        PacketPlayOutNamedSoundEffect sound = new PacketPlayOutNamedSoundEffect(CraftSound.getSound(hurtSound), loc.getX(), loc.getY(), loc.getZ(), 1.0F, 1.0F);
        for (Player player : world.getPlayers()) {
            PlayerConnection connection = ((CraftPlayer) player).getHandle().playerConnection;
            connection.sendPacket(anim);
-           connection.sendPacket(sound);
        }
     }
 
