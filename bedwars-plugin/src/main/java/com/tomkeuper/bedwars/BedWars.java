2f6af08a34edc525204c99e789680765ab003ea6
Yenil <85287520+ImYenil@users.noreply.github.com>
2025-11-19 18:11:15 +0900

    Added support for version 1.21.10 in BedWars class as it was missing

--- bedwars-plugin/src/main/java/com/tomkeuper/bedwars/BedWars.java
+++ bedwars-plugin/src/main/java/com/tomkeuper/bedwars/BedWars.java
@@ -245,6 +245,9 @@ public class BedWars extends JavaPlugin {
             case "1.21.8":
                 nmsVersion = "v1_21_R5";
                 break;
+            case "1.21.10":
+                nmsVersion = "v1_21_R6";
+                break;
             default:
                 break;
         }
