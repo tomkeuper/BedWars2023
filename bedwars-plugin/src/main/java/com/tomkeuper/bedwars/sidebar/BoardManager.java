20421daecb7ee87b9dc68bfd838cda60b54ae3c4
Tom Keuper <tomkeuper@live.nl>
2025-12-01 13:40:40 +0100

    Update temporary group handling in BoardManager to use "none" for lobby scoreboard

--- bedwars-plugin/src/main/java/com/tomkeuper/bedwars/sidebar/BoardManager.java
+++ bedwars-plugin/src/main/java/com/tomkeuper/bedwars/sidebar/BoardManager.java
@@ -377,7 +377,7 @@ public class BoardManager implements IScoreboardService {
             // Set scoreboard name and temporary group based on arena status
             if (arenaStatus == null) {
                 if (BedWars.config.getBoolean(ConfigPath.SB_CONFIG_SIDEBAR_USE_LOBBY_SIDEBAR)) scoreboardName = "bw_lobby_" + playerLanguage.getIso();
-                tabPlayer.setTemporaryGroup(null); // Clear temporary group to allow TAB to use permission-based groups
+                tabPlayer.setTemporaryGroup("none"); // Clear temporary group to allow TAB to use permission-based groups
             } else {
                 String temporaryGroup = null;
                 switch (arenaStatus) {
