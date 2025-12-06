8eb9893900c58d4cf64e7ae4ada811cf1993d9bf
Tom Keuper <tomkeuper@live.nl>
2025-12-07 00:03:39 +0100

    Rename quick_buy_2 table to quick_buy in SQLite schema

--- bedwars-plugin/src/main/java/com/tomkeuper/bedwars/database/SQLite.java
+++ bedwars-plugin/src/main/java/com/tomkeuper/bedwars/database/SQLite.java
@@ -87,7 +87,7 @@ public class SQLite implements IDatabase {
                 statement.executeUpdate(sql);
             }
             try (Statement st = connection.createStatement()) {
-                sql = "CREATE TABLE IF NOT EXISTS quick_buy_2 (uuid VARCHAR(36) PRIMARY KEY, " +
+                sql = "CREATE TABLE IF NOT EXISTS quick_buy (uuid VARCHAR(36) PRIMARY KEY, " +
                         "slot_19 VARCHAR(200), slot_20 VARCHAR(200), slot_21 VARCHAR(200), slot_22 VARCHAR(200), slot_23 VARCHAR(200), slot_24 VARCHAR(200), slot_25 VARCHAR(200)," +
                         "slot_28 VARCHAR(200), slot_29 VARCHAR(200), slot_30 VARCHAR(200), slot_31 VARCHAR(200), slot_32 VARCHAR(200), slot_33 VARCHAR(200), slot_34 VARCHAR(200)," +
                         "slot_37 VARCHAR(200), slot_38 VARCHAR(200), slot_39 VARCHAR(200), slot_40 VARCHAR(200), slot_41 VARCHAR(200), slot_42 VARCHAR(200), slot_43 VARCHAR(200));";
