447e3ab7308bfea1cedac5e861fe743f075ecad6
abbflaabb <callmeabbascallmeabbas@gmail.com>
2026-02-11 01:34:41 +0300

    just register the ArenaListener

--- bedwars-plugin/src/main/java/com/tomkeuper/bedwars/BedWars.java
+++ bedwars-plugin/src/main/java/com/tomkeuper/bedwars/BedWars.java
@@ -381,7 +381,7 @@ public class BedWars extends JavaPlugin {
 
         // Register events
         registerEvents(new EnderPearlLanded(), new QuitAndTeleportListener(), new BreakPlace(), new DamageDeathMove(), new Inventory(), new Interact(), new RefreshGUI(), new HungerWeatherSpawn(), new CmdProcess(),
-                new FireballListener(), new EggBridge(), new SpectatorListeners(), new BaseListener(), new TargetListener(), new LangListener(), new Warnings(this), new ChatAFK(), new GameEndListener());
+                new FireballListener(), new EggBridge(), new SpectatorListeners(), new BaseListener(), new TargetListener(), new LangListener(), new Warnings(this), new ChatAFK(), new GameEndListener(), new ArenaListener(this, invisConfig));
 
         if (config.getBoolean(ConfigPath.GENERAL_CONFIGURATION_HEAL_POOL_ENABLE)) {
             registerEvents(new HealPoolListener());
