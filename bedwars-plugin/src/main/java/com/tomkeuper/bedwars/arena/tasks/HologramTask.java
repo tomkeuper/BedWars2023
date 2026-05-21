b4b01c333c992fef998124fa210ad389ad98d265
sebastiaan den hertog <Sebastiaan.denhertog@gmail.com>
2026-05-21 14:10:22 +1200

    update to fix the hologram will be triggered synchronously error

--- bedwars-plugin/src/main/java/com/tomkeuper/bedwars/arena/tasks/HologramTask.java
+++ bedwars-plugin/src/main/java/com/tomkeuper/bedwars/arena/tasks/HologramTask.java
@@ -28,21 +28,28 @@ public class HologramTask implements Runnable {
                 World world = a.getWorld();
                 if (world == null) continue;
                 if (a.getStatus() != GameState.playing) continue;
-
                 if (world.getPlayers().isEmpty()) continue;
 
                 for (Player p : world.getPlayers()) {
                     if (p == null || !p.isOnline()) continue;
+
                     String iso = Language.getPlayerLanguage(p).getIso();
                     Location pLoc = p.getLocation();
                     List<ShopHolo> shopHolos = a.getShopHolograms(iso);
-                    if (shopHolos == null)  continue;
+                    if (shopHolos == null) continue;
+
                     for (ShopHolo shopHolo : shopHolos) {
                         if (shopHolo == null) continue;
+
                         IHologram hologram = shopHolo.getHologram();
+                        if (hologram == null) continue;
+
                         Location holoLoc = hologram.getLocation();
+                        if (holoLoc == null || holoLoc.getWorld() != pLoc.getWorld()) continue;
+
                         double distance = pLoc.distance(holoLoc);
-                        if (distance <= BedWars.hologramUpdateDistance) continue;
+                        if (distance > BedWars.hologramUpdateDistance) continue;
+
                         shopHolo.update(p);
                     }
                 }
@@ -51,37 +58,58 @@ public class HologramTask implements Runnable {
                     for (Player p : team.getMembers()) {
                         if (p == null || !p.isOnline()) continue;
                         if (p.getWorld() != world) continue;
+
                         String iso = Language.getPlayerLanguage(p).getIso();
                         IBedHolo bedHolo = team.getBedHologram(iso);
-                        if (bedHolo == null) continue;
+                        if (bedHolo == null || bedHolo.getHologram() == null) continue;
+
                         Location bedLoc = bedHolo.getHologram().getLocation();
                         Location pLoc = p.getLocation();
+                        if (bedLoc == null || bedLoc.getWorld() != pLoc.getWorld()) continue;
+
                         double distance = pLoc.distance(bedLoc);
 
-                        if (distance <= 4) bedHolo.hide(p);
-                        else if (distance > 4 && distance <= 8) bedHolo.show(p);
+                        if (distance <= 4) {
+                            bedHolo.hide(p);
+                        } else if (distance <= 8) {
+                            bedHolo.show(p);
+                        }
 
-                        if (distance >= BedWars.hologramUpdateDistance) bedHolo.update(p);
+                        if (distance <= BedWars.hologramUpdateDistance) {
+                            bedHolo.update(p);
+                        }
                     }
                 }
 
                 List<IGenerator> generators = a.getOreGenerators();
                 for (IGenerator generator : generators) {
+                    if (generator == null) continue;
+
                     GeneratorType type = generator.getType();
-                    if (type != GeneratorType.EMERALD && type !=  GeneratorType.DIAMOND) continue;
+                    if (type != GeneratorType.EMERALD && type != GeneratorType.DIAMOND) continue;
+
                     Location genLoc = generator.getLocation();
+                    if (genLoc == null) continue;
+
                     for (Player p : world.getPlayers()) {
                         if (p == null || !p.isOnline()) continue;
+
+                        Location pLoc = p.getLocation();
+                        if (pLoc.getWorld() != genLoc.getWorld()) continue;
+
+                        double distance = pLoc.distance(genLoc);
+                        if (distance > BedWars.hologramUpdateDistance) continue;
+
                         String iso = Language.getPlayerLanguage(p).getIso();
                         IGenHolo holo = generator.getLanguageHolograms().get(iso);
-                        if (holo == null) continue;
+                        if (holo != null) {
+                            holo.update(p);
+                        }
+
                         GeneratorHolder holder = generator.getHologramHolder();
-                        Location pLoc = p.getLocation();
-                        double distance = pLoc.distance(genLoc);
-                        if (distance <= BedWars.hologramUpdateDistance) continue;
-                        holo.update(p);
-                        if (holder == null) continue;
-                        holder.update(p);
+                        if (holder != null) {
+                            holder.update(p);
+                        }
                     }
                 }
             }
